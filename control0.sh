f8f9d8f0f897cd56bef0f067a9feaa84d7f497b5
waner1998 <106228384+waner1998@users.noreply.github.com>
2025-12-27 04:13:58 +0900

    Update control0.sh with new script commands (how to use)
    Replaced existing commands with new parameters and added validation and testing scripts.

--- control0.sh
+++ control0.sh
@@ -1,3 +1,4 @@
-python main.py --gpu_choose 0 --data_type 3 --net_type 11 --log_name outer --run_times 5
-python main.py --gpu_choose 0 --data_type 2 --net_type 11 --log_name outer --run_times 3
-
+python main.py --gpu_choose 0 --data_type 0 --net_type 4 --log_name outer --run_times 1
+python main.py --gpu_choose 0 --data_type 0 --net_type 4 --log_name outer --run_times 1
+python val.py
+python test.py
