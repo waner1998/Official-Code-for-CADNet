e891f5c314d39e4ae285b04c7b8f39f3132b6d09
waner1998 <106228384+waner1998@users.noreply.github.com>
2025-12-27 04:15:01 +0900

    How to use the code

--- control0.sh
+++ control0.sh
@@ -1,4 +1,2 @@
 python main.py --gpu_choose 0 --data_type 0 --net_type 4 --log_name outer --run_times 1
 python main.py --gpu_choose 0 --data_type 0 --net_type 4 --log_name outer --run_times 1
-python val.py
-python test.py
